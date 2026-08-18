fc85189248051d09af7eaa2ddac245b62074b99e
Negar Toosi <negar.tusi@gmail.com>
2026-08-18 13:50:27 +0330

    fix: resolve circular import in urls

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,5 +2,4 @@ from django.urls import include, path
 
 urlpatterns = [
     path("product/", include(("apps.product.urls", "product"))),
-    path("api/", include(("apps.api.urls", "api"))),
 ]
--- config/django/base.py
+++ config/django/base.py
@@ -37,9 +37,9 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    'apps.api',
-    'apps.core'
-    'apps.product',
+    'apps.api.apps.ApiConfig',
+    'apps.core.apps.CoreConfig',
+    'apps.product.apps.ProductConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
