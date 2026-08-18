b680d98315f686cfd2ba9826fc231c9f71553f4b
Negar Toosi <negar.tusi@gmail.com>
2026-08-18 10:10:57 +0330

    fix: delete pycache from git cache

--- apps/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, path
-
-urlpatterns = [
-    path("product/", include(("apps.product.urls", "product"))),
-    path("api/", include(("apps.api.urls", "api"))),
-]
--- config/settings/dev.py
+++
@@ -1,3 +0,0 @@
-from config.settings.base import *
-
-DEBUG = True
