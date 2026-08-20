174ba5c40cf4d3e6849b4b91e60a9406a5269dfe
negar-toosi <144216039+negar-toosi@users.noreply.github.com>
2026-08-20 17:31:28 +0330

    chore: add inventory exception handler to drf global setting

--- config/settings/drf.py
+++ config/settings/drf.py
@@ -1,4 +1,5 @@
 REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "apps.core.exception_handlers.inventory_exception_handler",
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [],
     "DEFAULT_PERMISSION_CLASSES": [
