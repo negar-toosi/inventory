bae672469fbbbb2ae46417b5ff994831c4ff87f8
negar-toosi <144216039+negar-toosi@users.noreply.github.com>
2026-08-20 19:40:10 +0330

    feat: change product inventory API

--- apps/product/api/views.py
+++ apps/product/api/views.py
@@ -2,12 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework import status
 from drf_spectacular.utils import extend_schema
-from apps.product.api.serializers import AddProductRequest, ProductSerializer
+from apps.product.api.serializers import AddProductRequest, ProductSerializer, InventoryTransactionRequest
 from apps.core.custom_response import CustomResponse
-from apps.product.services import ProductServices
+from apps.product.services import ProductServices, InventoryTransactionServices
+from uuid import UUID
 
-
-class AddProductAPI(APIView):
+class ProductAPI(APIView):
     @extend_schema(request=AddProductRequest, responses={201: ProductSerializer})
     def post(self, request: Request) -> CustomResponse:
         serializer = AddProductRequest(data=request.data)
@@ -22,4 +22,21 @@ class AddProductAPI(APIView):
             message="Product Created Successfully",
             data=response.data,
             status_code=status.HTTP_201_CREATED,
+        )
+
+class InventoryTransactionAPI(APIView):
+    @extend_schema(request=InventoryTransactionRequest, responses=ProductSerializer)
+    def post(self, request: Request, id: UUID) -> CustomResponse:
+        serializer = InventoryTransactionRequest(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+
+        new_product = InventoryTransactionServices.add(product_id=id,**validated_data)
+
+        response = ProductSerializer(new_product)
+        
+        return CustomResponse(
+            message="Product Updated Successfully",
+            data=response.data,
+            status_code=status.HTTP_200_OK,
         )
--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.product.api.views import AddProductAPI
+from apps.product.api.views import ProductAPI, InventoryTransactionAPI
 
 urlpatterns = [
-    path("add/", AddProductAPI.as_view(), name="add-new-product"),
+    path("add/", ProductAPI.as_view(), name="add-new-product"),
+    path("<uuid:id>/inventory/transactions/", InventoryTransactionAPI.as_view(), name="inventory-transaction")
 ]
