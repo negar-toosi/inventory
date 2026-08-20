c3b3b0412e9fb656515315e7b33600297c7fece1
negar-toosi <144216039+negar-toosi@users.noreply.github.com>
2026-08-20 16:57:12 +0330

    feat: add product creation API endpoint

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import include, path
+
+from apps.product.api.views import AddProductAPI
+urlpatterns = [
+    path("add/", AddProductAPI.as_view(), name='add-new-product'),
+]
