d06bd3514905fd65b9ae8839bf24599a2bd4c756
negar-toosi <144216039+negar-toosi@users.noreply.github.com>
2026-08-20 12:09:58 +0330

    feat: add request and response serializer for inventory transaction api

--- apps/product/api/serializers.py
+++ apps/product/api/serializers.py
@@ -1,4 +1,12 @@
 from rest_framework import serializers
+from apps.product.enums import InventoryTransactionType
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    name = serializers.CharField()
+    current_inventory = serializers.IntegerField()
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
 
 
 class AddProductRequest(serializers.Serializer):
@@ -6,9 +14,9 @@ class AddProductRequest(serializers.Serializer):
     quantity = serializers.IntegerField(min_value=0)
 
 
-class AddProductResponse(serializers.Serializer):
-    id = serializers.UUIDField()
-    name = serializers.CharField()
-    current_inventory = serializers.IntegerField()
-    created_at = serializers.DateTimeField()
-    updated_at = serializers.DateTimeField()
+class InventoryTransactionRequest(serializers.Serializer):
+    quantity = serializers.IntegerField(min_value=1)
+    type = serializers.ChoiceField(
+        choices=InventoryTransactionType.choices
+    )
+
--- apps/product/api/views.py
+++ apps/product/api/views.py
@@ -2,13 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework import status
 from drf_spectacular.utils import extend_schema
-from apps.product.api.serializers import AddProductRequest, AddProductResponse
+from apps.product.api.serializers import AddProductRequest, ProductSerializer
 from apps.core.custom_response import CustomResponse
 from apps.product.services import ProductServices
 
 
 class AddProductAPI(APIView):
-    @extend_schema(request=AddProductRequest, responses={201: AddProductResponse})
+    @extend_schema(request=AddProductRequest, responses={201: ProductSerializer})
     def post(self, request: Request) -> CustomResponse:
         serializer = AddProductRequest(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -16,7 +16,7 @@ class AddProductAPI(APIView):
 
         product = ProductServices.add(**validated_data)
 
-        response = AddProductResponse(product)
+        response = ProductSerializer(product)
 
         return CustomResponse(
             message="Product Created Successfully",
