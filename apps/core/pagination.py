f5e826c81f0a60ccc40bac49146b607a8a51287f
negar-toosi <144216039+negar-toosi@users.noreply.github.com>
2026-08-20 20:59:19 +0330

    refactor: simplify pagination to page number

--- apps/core/pagination.py
+++ apps/core/pagination.py
@@ -1,21 +1,9 @@
-from rest_framework.pagination import (
-    LimitOffsetPagination as _LimitOffsetPagination,
-    PageNumberPagination as _PageNumberPagination,
-)
-
+from rest_framework.pagination import PageNumberPagination as _PageNumberPaginaton
 from apps.core.custom_response import CustomResponse
 
 
-def get_paginated_response(
-    *,
-    message,
-    pagination_class,
-    serializer_class,
-    queryset,
-    request,
-    view,
-):
-    paginator = pagination_class()
+def get_paginated_response(*, message, serializer_class, queryset, request, view):
+    paginator = PageNumberPagination()
 
     page = paginator.paginate_queryset(queryset, request, view=view)
 
@@ -24,44 +12,19 @@ def get_paginated_response(
         return paginator.get_paginated_response(serializer.data)
 
     serializer = serializer_class(queryset, many=True)
-
-    return CustomResponse(
-        data=serializer.data,
-        message=message,
-    )
+    return CustomResponse(data=serializer.data, message=message)
 
 
-class CustomPageNumberPagination(_PageNumberPagination):
+class PageNumberPagination(_PageNumberPaginaton):
     page_size = 10
-    page_size_query_param = "page_size"
-    max_page_size = 50
 
     def get_paginated_response(self, data):
         return CustomResponse(
-            data=data,
-            message="Data retrieved successfully.",
-            extra={
-                "total": self.page.paginator.count,
-                "page": self.page.number,
-                "page_size": self.page.paginator.per_page,
-                "total_pages": self.page.paginator.num_pages,
-            },
-        )
-
-
-class LimitOffsetPagination(_LimitOffsetPagination):
-    default_limit = 10
-    max_limit = 50
-
-    def get_paginated_response(self, data):
-        return CustomResponse(
-            data=data,
-            message="Data retrieved successfully.",
-            extra={
-                "limit": self.limit,
-                "offset": self.offset,
-                "count": self.count,
+            {
+                "count": self.page.paginator.count,
                 "next": self.get_next_link(),
                 "previous": self.get_previous_link(),
-            },
+                "total_pages": self.page.paginator.num_pages,
+                "results": data,
+            }
         )
