48ee4361ff21f81b0737f077673ff5c1c33086a1
negar-toosi <144216039+negar-toosi@users.noreply.github.com>
2026-08-20 12:38:06 +0330

    feat: implement api for add new product

---
+++ apps/product/api/views.py
@@ -0,0 +1,25 @@
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework import status
+from drf_spectacular.utils import extend_schema
+from apps.product.api.serializers import AddProductRequest, AddProductResponse
+from apps.core.custom_response import CustomResponse
+from apps.product.services import ProductServices
+
+
+class AddProductAPI(APIView):
+    @extend_schema(request=AddProductRequest, responses={201: AddProductResponse})
+    def post(self, request: Request) -> CustomResponse:
+        serializer = AddProductRequest(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+
+        product = ProductServices.add(**validated_data)
+
+        response = AddProductResponse(product)
+
+        return CustomResponse(
+            message="Product Created Successfully",
+            data=response.data,
+            status_code=status.HTTP_201_CREATED,
+        )
