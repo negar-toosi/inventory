ac954935cc1169ec0c9ce579a7a18ddb8ff6369e
negar-toosi <144216039+negar-toosi@users.noreply.github.com>
2026-08-20 16:55:17 +0330

    feat: add product models to admin panel

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,1 +1,19 @@
-# Register your models here.
+from django.contrib import admin
+from apps.product.models import Product, InventoryTransaction
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "current_inventory",
+        "is_deleted",
+    )
+
+@admin.register(InventoryTransaction)
+class InventoryTransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "product",
+        "quantity",
+        "current_inventory",
+        "type",
+    )
